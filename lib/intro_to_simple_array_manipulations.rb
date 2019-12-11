73a2487a9178ca288bf3710e49cf4f78d20faed9
Reese M Bowman <reesemb1@gmail.com>
2019-12-11 21:40:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array1, element)
-  array1.insert(3, element)
+def using_insert(array, element)
+  array.insert(3, element)
 end
 
 def using_uniq(array)
